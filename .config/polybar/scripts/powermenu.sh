c9e86ebd1e8cb09c63579e64c398f736f5fb4cf6
Michael Aerni <michael.aerni@hotmail.com>
2020-06-30 10:05:58 +0200

    Added sleep and hibernate to power menu

--- .config/polybar/scripts/powermenu.sh
+++ .config/polybar/scripts/powermenu.sh
@@ -2,13 +2,16 @@
 
 MENU="$(rofi -no-lazy-grab -sep "|" -dmenu -i -p 'System :' \
 -hide-scrollbar true \
--lines 3 \
+-me-select-entry '' -me-accept-entry 'MousePrimary' \
+-lines 5 \
 -width 15 \
 -xoffset 0 -yoffset 24 \
 -location 3 \
-<<< ">> Lock|>> Reboot|>> Shutdown")"
+<<< ">> Lock|>> Sleep|>> Hibernate|>> Reboot|>> Shutdown")"
 case "$MENU" in
   *Lock) dm-tool lock ;;
+  *Sleep) systemctl suspend ;;
+  *Hibernate) notify-send "Hibernate" "Hibernate is not configured and implemented yet" -a "Power Menu" ;; # TODO: Implement hibernate
   *Reboot) systemctl reboot ;;
   *Shutdown) systemctl -i poweroff
 esac
